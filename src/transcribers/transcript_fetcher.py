eb2a3ed486a044f41b25aa42ef50fcf7c67dfe5b
Aryan-B2107 <aryanbalikondwar@gmail.com>
2025-06-12 22:25:16 +0530

    Update transcript_fetcher.py

--- src/transcribers/transcript_fetcher.py
+++ src/transcribers/transcript_fetcher.py
@@ -1,9 +1,30 @@
-class Operations:
-    def add(self, num1, num2):
-        return num1+num2
-    def subtract(self, num1, num2):
-        return num1-num2
-    def divide(self, num1, num2):
-        return num1/num2
-    def multiply(self, num1, num2):
-        return num1*num2
+"""
+Initially for the sake of working, I'm going to fetch the transcript using YouTubeTranscriptApi.
+
+But Eventually the following will be implemented:
+1)Use Beautiful soup to get html of a give YouTube URL
+2)scrape the relevant transcript part through analysing css formats, getting all the relevant data like
+time stamps and the transcript for that time stamp
+3) write in a clean json file and return the json file
+
+"""
+
+"""
+OKAY.. SHIT, so youtube transcript API doesn't work anymore
+and I wasted two days for this shit.
+
+It's cause AI companies were putting too much load on youtube's servers
+by millions of requests, so youtube fought back and now we can't use it
+
+Guess the user will have to manually copy paste transcripts,
+which is still faster ngl
+
+ALSO:
+Many requests can cause IP bans, so this method is generally risky
+
+Though we can work around using multiple proxy servers.
+
+
+"""
+
+
