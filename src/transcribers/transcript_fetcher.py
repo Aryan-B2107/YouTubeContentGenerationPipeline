871d164a8059af6a66234879fc5cc3f997825ff7
Aryan-B2107 <aryanbalikondwar@gmail.com>
2025-06-14 11:15:44 +0530

    Update transcript_fetcher.py

--- src/transcribers/transcript_fetcher.py
+++ src/transcribers/transcript_fetcher.py
@@ -10,19 +10,18 @@ time stamps and the transcript for that time stamp
 """
 
 """
-OKAY.. SHIT, so youtube transcript API doesn't work anymore
-and I wasted two days for this shit.
+OKAY..  so youtube transcript API doesn't work anymore
 
 It's cause AI companies were putting too much load on youtube's servers
 by millions of requests, so youtube fought back and now we can't use it
 
 Guess the user will have to manually copy paste transcripts,
-which is still faster ngl
+which is still faster
 
 ALSO:
 Many requests can cause IP bans, so this method is generally risky
 
-Though we can work around using multiple proxy servers.
+Though we can work around that using multiple proxy servers.
 
 
 """
